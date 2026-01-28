6d950fe8ff64d60a860c7abfd20a1923b3b90c67
Adarsh Kumar Pal <paladarsh978@gmail.com>
2026-01-28 11:55:49 +0530

    project set up

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
